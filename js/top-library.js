6009bd9f15792224d7fdb39d28f7a345b7c45bc7
Kenya Chan <kenya.chan@gmail.com>
2022-10-06 20:36:34 +1100

    Define book objects

--- js/top-library.js
+++ js/top-library.js
@@ -1,9 +1,19 @@
 let myLibrary = [];
 
-function Book() {
-
+function Book(title, cover, author, pages) {
+    this.title = title;
+    this.cover = cover;
+    this.author = author;
+    this.pages = pages;
 }
 
+function MyBook(read, readPages) {
+    this.read = read;
+    this.readPages = readPages;
+}
+
+MyBook.prototype = Object.create(Book.prototype);
+
 function addBookToLibrary(book) {
-    myLibrary.push(book);    
+    myLibrary.push(book);
 }
