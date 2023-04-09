2d0873212b913a8e409169d4635234dab3f1d888
Kenya Chan <kenya.chan@gmail.com>
2023-04-09 21:29:11 +1000

    Config webpack to look in ./dist

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,14 @@ const path = require('path');
 
 module.exports = {
 	entry: './src/index.js',
+	devServer: {
+		static: './dist',
+	},
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
+	},
+	optimization: {
+		runtimeChunk: 'single',
 	},
 };
