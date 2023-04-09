9cb27e97230630e0764b032973cb58e51dc6b78a
Kenya Chan <kenya.chan@gmail.com>
2023-04-09 21:34:42 +1000

    Config html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
 	devServer: {
 		static: './dist',
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'library',
+		}),
+	],
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
